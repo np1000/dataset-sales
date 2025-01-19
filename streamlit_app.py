ec3dfbb13ae39afa7664509fd3f204ad765fe7e5
np1000 <neelima257@yahoo.com>
2025-01-18 18:05:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,18 @@ import time
 
 st.title('🎈 App Name')
 
-st.write('Hello world!')
+@st.cache_data
+def get_data():
+    """Generate random sales data for Widget A through Widget Z"""
+
+    product_names = ["Widget " + letter for letter in string.ascii_uppercase]
+    average_daily_sales = np.random.normal(1_000, 300, len(product_names))
+    products = dict(zip(product_names, average_daily_sales))
+
+    data = pd.DataFrame({})
+    sales_dates = np.arange(date(2023, 1, 1), date(2024, 1, 1), timedelta(days=1))
+    for product, sales in products.items():
+        data[product] = np.random.normal(sales, 300, len(sales_dates)).round(2)
+    data.index = sales_dates
+    data.index = data.index.date
+    return data
